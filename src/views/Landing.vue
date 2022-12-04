336b7789b1445a64fdd978ce7276cfce094b4562
asadahimeka <smallstar9637@yahoo.co.jp>
2022-12-05 02:10:11 +0800

    fix: home bg pos

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -142,7 +142,7 @@ export default {
           }, 200)
           img = null
         }
-        img.src = bg
+        // img.src = bg
       } catch (error) {
         this.landingBG = this.staticLandingBG
       }
@@ -298,7 +298,7 @@ export default {
     max-width: 100vw;
     height: 100vh;
     border-radius: 0;
-    background-position: center top;
+    background-position: center center;
     background-size: cover;
     filter: none;
 
