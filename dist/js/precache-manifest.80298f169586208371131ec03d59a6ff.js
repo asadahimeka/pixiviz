2ba581433369370aeae28bb46975d1e9b8e7044d
asadahimeka <smallstar9637@yahoo.co.jp>
2023-07-09 17:08:35 +0800

    add dist files

---
+++ dist/js/precache-manifest.80298f169586208371131ec03d59a6ff.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1191f741f05ab6643078",
+    "url": "/css/app.6877f79f.css"
+  },
+  {
+    "revision": "6eb3f7ace168cf60c5145787b6fdbc96",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "722c22e06babd7ddae0b93b4dde1c2ed",
+    "url": "/fonts/element-icons-common.722c22e0.woff2"
+  },
+  {
+    "revision": "105a65aefa1d1644d4e969a88dfa6ab6",
+    "url": "/fonts/element-icons-extra.105a65ae.woff2"
+  },
+  {
+    "revision": "8e471ebe5583d63de7bec6872f339da4",
+    "url": "/img/98606454.8e471ebe.jpg"
+  },
+  {
+    "revision": "f2cb643c4302744194bb46c055df2a7d",
+    "url": "/img/back.f2cb643c.svg"
+  },
+  {
+    "revision": "cfb3875968f77aa996c0859fc40826e0",
+    "url": "/img/home.cfb38759.svg"
+  },
+  {
+    "revision": "61fc9c526f7f36f5639d79ca40ddbb66",
+    "url": "/img/link-inline.61fc9c52.svg"
+  },
+  {
+    "revision": "06261fa0135d1d2e9aab94c6d8e84d87",
+    "url": "/img/link.06261fa0.svg"
+  },
+  {
+    "revision": "fa29bb9b0b93fee7b167ae1ace589d16",
+    "url": "/img/rank.fa29bb9b.svg"
+  },
+  {
+    "revision": "9c8d968ab0af437363b912f7d7f84d4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1191f741f05ab6643078",
+    "url": "/js/app.48c5b56c.js"
+  },
+  {
+    "revision": "5b0328db1c1b494ff8d2",
+    "url": "/js/chunk-basic.b19ad6df.js"
+  },
+  {
+    "revision": "f22dd3060790601786e7",
+    "url": "/js/chunk-core.659b076f.js"
+  },
+  {
+    "revision": "aa1085d10053bcb64e3e",
+    "url": "/js/chunk-element.846855a5.js"
+  },
+  {
+    "revision": "de74a4c498fc164d3c3b",
+    "url": "/js/chunk-vendors.4022b754.js"
+  },
+  {
+    "revision": "a76bc9411227049e6ec1c1a2aab462c3",
+    "url": "/js/gif.js"
+  },
+  {
+    "revision": "7a0f8ebbb7f041fd0f2b85cb7a014f59",
+    "url": "/js/gif.worker.js"
+  },
+  {
+    "revision": "812327dd0011eeb6e771e97871dee7da",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "8e78557b21dd2e35cd77d86c47d5efe5",
+    "url": "/robots.txt"
+  }
+]);
---
+++ dist/service-worker.js
@@ -0,0 +1,40 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("/js/workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/js/workbox-v4.3.1"});
+
+importScripts(
+  "/js/precache-manifest.80298f169586208371131ec03d59a6ff.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "pixiviz"});
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/^https:\/\/pixiviz\.kanata\.ml(\/.*\.(html|js|css))?$/, new workbox.strategies.NetworkFirst({ "cacheName":"static-files","networkTimeoutSeconds":10, plugins: [new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] }), new workbox.expiration.Plugin({ maxAgeSeconds: 604800, purgeOnQuotaError: false })] }), 'GET');
+workbox.routing.registerRoute(/^https:\/\/pixiviz\.kanata\.ml(\/.*\.(jpg|jpeg|png|webp|svg))?$/, new workbox.strategies.CacheFirst({ "cacheName":"static-imgs", plugins: [new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] }), new workbox.expiration.Plugin({ maxAgeSeconds: 259200, purgeOnQuotaError: false })] }), 'GET');
+workbox.routing.registerRoute(/^(https:\/\/hm\.baidu\.com\/hm\.js)|(https:\/\/cdn-go\.cn\/aegis\/aegis-sdk\/latest\/aegis\.min\.js)/, new workbox.strategies.CacheFirst({ "cacheName":"stat-files", plugins: [new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] }), new workbox.expiration.Plugin({ maxAgeSeconds: 259200, purgeOnQuotaError: false })] }), 'GET');
+workbox.routing.registerRoute(/^https:\/\/((pixiviz\.pwp\.app\/api\/)|(pixiviz-api-[a-z]{2}\.pwp\.link\/)).+/, new workbox.strategies.CacheFirst({ "cacheName":"api-return", plugins: [new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] }), new workbox.expiration.Plugin({ maxAgeSeconds: 604800, maxEntries: 30000, purgeOnQuotaError: false })] }), 'GET');
+workbox.routing.registerRoute(/^https:\/\/gfonts\.pwp\.link\/.*$/, new workbox.strategies.CacheFirst({ "cacheName":"gfont","fetchOptions":{"credentials":"omit","mode":"cors"}, plugins: [new workbox.cacheableResponse.Plugin({ statuses: [ 200 ] }), new workbox.expiration.Plugin({ maxAgeSeconds: 7776000, maxEntries: 500, purgeOnQuotaError: false })] }), 'GET');
+workbox.routing.registerRoute(/^https:\/\/pic1\.afdiancdn\.com\/.*$/, new workbox.strategies.CacheFirst({ "cacheName":"sponsor-avatar","fetchOptions":{"credentials":"omit","mode":"cors"}, plugins: [new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] }), new workbox.expiration.Plugin({ maxAgeSeconds: 86400, maxEntries: 50, purgeOnQuotaError: false })] }), 'GET');
